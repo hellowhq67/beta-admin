c8b0ab48e9d39d65434567d30680df36187def92
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-03-02 01:18:17 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,6 +12,16 @@ const nextConfig = {
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
       }
+       {
+        // matching all API routes
+        source: "/api/products/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://beta-admin-two.vercel.app" }, // Set specific origin
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
     ]
   }
 };
